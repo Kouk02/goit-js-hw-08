63f35d993b2839ea1d6005a26de2c6e757e63d78
Kouk02 <kouk20020703@gmail.com>
2023-10-24 13:23:27 +0300

    update comit 1.0.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+const timeupdate = function (data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+};
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
+player.on('timeupdate', throttle(timeupdate, 1000));
