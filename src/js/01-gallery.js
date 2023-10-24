faf24d78508e09906727df4d690797278b3f8c92
Kouk02 <kouk20020703@gmail.com>
2023-10-24 04:54:38 +0300

    update comit 1.0.0

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
+import SimpleLightbox from 'simplelightbox'
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+  </li>`
+}).join("")
+console.log(galleryMarkup)
+
+const galleryList = document.querySelector('.gallery');
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup)
